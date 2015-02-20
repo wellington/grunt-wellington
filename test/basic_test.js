293a5edfe0a514e406290aeb593907d40837baff
Drew Wells <drew.wells00@gmail.com>
2015-02-20 09:52:52 -0600

    disable sass test since it's not on 0.6.0

--- test/basic_test.js
+++ test/basic_test.js
@@ -10,12 +10,14 @@ exports.wt = {
     test.ok(/border-color: #3bbfce/.test(grunt.file.read('tmp/compile.css')), 'should compile Sass to CSS');
 
     test.done();
-  },
-  sass: function (test) {
+  }//,
+  // Skip this test until wt release with .sass parsing
+  /*sass: function (test) {
+
     test.expect(1);
 
-    test.ok(/color: red/.test(grunt.file.read('tmp4/simple.sass')), 'should compile Sass to CSS');
+    test.ok(/color: red/.test(grunt.file.read('tmp4/simple.css')), 'should compile Sass to CSS');
 
     test.done();
-  }
+  }*/
 };
